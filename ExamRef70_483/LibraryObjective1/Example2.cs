e0a6bbdb535698e853db3599c29a7b8bd58b59b6
VU Tuan Trung <trung.lespace@gmail.com>
2020-03-10 17:18:57 +0100

    Using a background/foreground thread

---
+++ ExamRef70_483/LibraryObjective1/Example2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibraryObjective1
+{
+    // Using background thread
+    public static class Example2
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void Example2_Main()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+
+            // If true, the application will exits immediatly
+            // If false (creating a foreground thread), the application prints the ThreadProc message 10 times
+            t.IsBackground = true;
+
+            t.Start();
+        }
+    }
+
+    // _The Foreground threads can be used to keep an application alive as long as they are running,
+    // when all foreground threads end does the common language runtime (CLR) shut down application.
+    // _The background threads are then terminated.
+    // _When all the foreground threads complete execution, the application will exit before the background threads return.
+    // _After all the foreground threads have been stopped, or after the application exitsm the system stops all background threads.
+}
