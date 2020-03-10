84ebbf99cb451c73c3a5d37771078214bb622803
VU Tuan Trung <trung.lespace@gmail.com>
2020-03-10 18:00:03 +0100

    Stopping a thread

---
+++ ExamRef70_483/LibraryObjective1/Example4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibraryObjective1
+{
+    // Stoping a thread
+    public class Example4 : IExample
+    {
+        public void Example4_Main()
+        {
+            bool stopped = false;
+
+            // The thread is initialized with a lambda expression
+            Thread t = new Thread(new ThreadStart(() =>
+            {
+                // this thread check variable stopped value to decide stopping or not
+                while (!stopped)
+                {
+                    Console.WriteLine("Running...");
+                    Thread.Sleep(1000);
+                }
+
+            }));
+
+            t.Start();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+            stopped = true;
+            t.Join();
+        }
+
+        public void Example_Execute() => Example4_Main();
+    }
+}
