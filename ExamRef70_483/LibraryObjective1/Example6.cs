985a4d0854a942f4d0055717560b4eec77b4bffb
VU Tuan Trung <trung.lespace@gmail.com>
2020-03-11 23:32:08 +0100

    Using ThreadLocal<T>

---
+++ ExamRef70_483/LibraryObjective1/Example6.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibraryObjective1
+{
+    // Using ThreadLocal<T>
+    public class Example6 : IExample
+    {
+        // This class allow us to declare a variable which is not shared between threads
+        // and one of his capabilities that we can initialize each instance of the variable as the class the supplied factory method to create and/or initialized value to be returned.
+        // Also, unlike ThreadStatic which is only works on static fields, ThreadLocal can be applied to static or instance variables.
+        public static ThreadLocal<int> _field = new ThreadLocal<int>(() =>
+        {
+            // This is a very useful tool because by asking this class,
+            // we can observe the current thread: its information, its state...
+            return Thread.CurrentThread.ManagedThreadId;
+        });
+
+        public void Main()
+        {
+            new Thread(() =>
+            {
+                for(int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine("Thread A: {0}", i);
+                }
+            });
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field.Value; i++)
+                {
+                    Console.WriteLine("Thread B: {0}", i);
+                }
+            });
+        }
+
+        public void Example_Execute() => Main();
+
+        // When a thread is created, the runtime ensures that the initiating thread’s execution context is flowed to the new thread.
+        // This way the new thread has the same privileges as the parent thread.
+    }
+}
