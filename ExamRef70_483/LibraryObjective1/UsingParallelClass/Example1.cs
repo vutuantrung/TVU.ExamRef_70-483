a8bcca5d39fcf3fc1024eaaf6d540ba0a53b1ebf
VU Tuan Trung <trung.lespace@gmail.com>
2020-03-18 18:03:22 +0100

    Using Parallel.For and Parallel.Foreach

---
+++ ExamRef70_483/LibraryObjective1/UsingParallelClass/Example1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibraryObjective1.UsingParallelClass
+{
+    // Using Parallel.For and Parallel.Foreach
+    public class Example1 : IExample
+    {
+        public void Main()
+        {
+            Parallel.For(0, 10, (i) =>
+            {
+                Thread.Sleep(1000);
+            });
+
+            var numbers = Enumerable.Range(0, 10);
+            Parallel.ForEach(numbers, (i) =>
+            {
+                Thread.Sleep(1000);
+            });
+        }
+
+        public void Example_Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
