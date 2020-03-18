392e01e4fd6008c49928a7d05345d7c1972795f3
VU Tuan Trung <trung.lespace@gmail.com>
2020-03-18 18:29:41 +0100

    Using Parallel.Break

---
+++ ExamRef70_483/LibraryObjective1/UsingParallelClass/Example2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryObjective1.UsingParallelClass
+{
+    // Using Paralle.Break
+    public class Example2 : IExample
+    {
+        public void Main()
+        {
+            var result = Parallel.For(0, 1000, (int i, ParallelLoopState loopState) =>
+            {
+                if (i == 500)
+                {
+                    Console.WriteLine($"Breaking loop at { loopState.LowestBreakIteration }.");
+                    loopState.Break();
+                }
+            });
+        }
+
+        public void Example_Execute() => Main();
+    }
+
+    // When breaking the parallel loop, the result variable has an IsCompleted value of false
+    // and LowestBreakIteration of 500.
+    // When you use the Stop method, the LowestBreakIteration is null
+}
